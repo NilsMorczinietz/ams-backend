a629327104c161518f42d678bd1d9e9abc134533
Nils Morczinietz <nils.morczinietz@vodafonemail.de>
2026-04-24 12:13:02 +0200

    Refactor UpdateSurveyDto: replace PartialType with explicit properties for name, description, type, startDate, and endDate

--- src/aggregates/survey/dto/update-survey.dto.ts
+++ src/aggregates/survey/dto/update-survey.dto.ts
@@ -1,4 +1,44 @@
-import { PartialType } from '@nestjs/swagger';
-import { CreateSurveyDto } from './create-survey.dto';
+import { ApiPropertyOptional } from '@nestjs/swagger';
+import { Transform } from 'class-transformer';
+import { IsDateString, IsEnum, IsOptional, IsString } from 'class-validator';
+import { SurveyType } from 'generated/prisma/client';
 
-export class UpdateSurveyDto extends PartialType(CreateSurveyDto) {}
+export class UpdateSurveyDto {
+  @IsOptional()
+  @IsString()
+  @Transform(({ value }: { value: string }) => value?.trim())
+  @ApiPropertyOptional({ example: 'Bestandsabfrage' })
+  name?: string;
+
+  @IsOptional()
+  @IsString()
+  @Transform(({ value }: { value: string }) => value?.trim())
+  @ApiPropertyOptional({
+    example: 'Hiermit werden alle bereits durchgeführten Lehrgänge abgefragt.',
+  })
+  description?: string;
+
+  @IsOptional()
+  @IsEnum(SurveyType)
+  @ApiPropertyOptional({
+    example: SurveyType.COURSE_INVENTORY,
+    enum: SurveyType,
+  })
+  type?: SurveyType;
+
+  @IsOptional()
+  @IsDateString()
+  @Transform(({ value }: { value: string }) => value?.trim())
+  @ApiPropertyOptional({
+    example: '2024-01-01T00:00:00.000Z',
+  })
+  startDate?: Date;
+
+  @IsOptional()
+  @IsDateString()
+  @Transform(({ value }: { value: string }) => value?.trim())
+  @ApiPropertyOptional({
+    example: '2024-12-31T23:59:59.000Z',
+  })
+  endDate?: Date;
+}
